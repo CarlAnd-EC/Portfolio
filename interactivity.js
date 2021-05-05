b5763d216b95ba053a465732967c2c798bc4dd0e
Carlos Andres Escalona Contreras <carlos.escalona@theksquaregroup.com>
2021-05-05 08:29:18 -0500

    Portfolio V1.8.2.2.7

--- interactivity.js
+++ interactivity.js
@@ -1,7 +1,5 @@
 window.onload = function() {
-  console.log("Page loaded...");
   var detailedSections = document.getElementsByClassName('summary-main-section');
-  console.log(detailedSections);
   for(let i = 0; i < detailedSections.length; i++) {
     const hr = `hr${i+1}`;
     const arrow = `arrow${i+1}`;
@@ -12,11 +10,7 @@ window.onload = function() {
       document.getElementById(arrow).classList.toggle("downwards");
     }
   }
-  // Get all elements with class="tabcontent" and hide them
-  description = document.getElementsByClassName("tabcontent");
-  for (let i = 0; i < description.length; i++) {
-    description[i].style.zIndex = `${i}`;
-  }
+  console.log("Page loaded...");
 }
 const main = document.getElementById("main");
 const sideNavbar = document.getElementById("sideNavbar");
@@ -27,8 +21,27 @@ const showForm = document.getElementById("showForm");
 const contFormSec = document.getElementById("contactFormSection");
 const contactForm = document.getElementById("contactForm");
 const submitMsg = document.getElementById("submitMsg");
+const homeBtn = document.getElementById("homeBtn");
 const dropBtn = document.getElementById("dropBtn");
 
+// Get all elements with class="tabcontent"
+description = document.getElementsByClassName("tabcontent");
+for (let i = 0; i < description.length; i++) {
+  description[i].style.zIndex = `${i}`;
+}
+function clickGlow(){
+  homeBtn.classList.toggle("active");
+  setTimeout(() => {
+    homeBtn.classList.toggle("active");
+  }, 200);
+}
+function highlightSelectedBtn(event,className){
+  const button = document.getElementsByClassName(className);
+  for (let i = 0; i < button.length; i++) {
+    button[i].classList.remove("active");
+  }
+  event.currentTarget.classList.add("active");
+}
 function openNav() {
   // sideNavbar.style.width = "100px";
   sideNavbar.style.height = "70vh";
@@ -38,7 +51,6 @@ function openNav() {
   showMenu.style.visibility = "hidden";
   hideMenu.style.visibility = "visible";
 }
-
 function closeNav() {
   // sideNavbar.style.width = "0";
   sideNavbar.style.height = "0";
@@ -47,7 +59,28 @@ function closeNav() {
   showMenu.style.visibility = "visible";
   hideMenu.style.visibility = "hidden";
 }
-
+function drop() {
+  document.getElementById("dropBtn").classList.toggle("active");
+  document.getElementById("dropdownContent").classList.toggle("active");
+}
+/*Vertical Tab Gallery (Interests section)*/
+function describe(event, topic){
+  if(event.currentTarget.classList.contains("active")){
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(topic).classList.toggle("active");
+    event.currentTarget.classList.toggle("active");
+  }
+  else{
+    const tab = document.getElementsByClassName("tablink");
+    const content = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tab.length; i++) {
+      tab[i].classList.remove("active");
+      content[i].classList.remove("active");
+    }
+    document.getElementById(topic).classList.add("active");
+    event.currentTarget.classList.add("active");
+  }
+}
 function showContactForm(){
   showForm.style.boxShadow = "none";
   showForm.classList.toggle("click");
@@ -60,7 +93,6 @@ function showContactForm(){
     contactForm.style.visibility = "visible";
   }, 1100);
 }
-
 function submitMsgEffect(){
   submitMsg.style.boxShadow = "none";
   submitMsg.classList.toggle("click");
@@ -71,21 +103,14 @@ function submitMsgEffect(){
     showForm.classList.toggle("click");
   }, 1100);
 }
-function drop() {
-  document.getElementById("dropBtn").classList.toggle("active");
-  document.getElementById("dropdownContent").classList.toggle("active");
-}
+
+
 hideMenu.onclick = () => closeNav();
 showMenu.onclick = () => openNav();
 showForm.onclick = () => showContactForm();
 submitMsg.onclick = () => submitMsgEffect();
+homeBtn.onclick = () => clickGlow();
 dropBtn.onclick = () => drop();
-/*Vertical Tab Gallery (Interests section)*/
-function describe(event, topic) {
-  // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(topic).classList.toggle("active");
-  event.currentTarget.classList.toggle("active");
-}
 
 /*Slideshow (Projects section) */
 var slideIndex = 1;
