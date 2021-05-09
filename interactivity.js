cd7547483780f35d3a41d0d7063157c875794d35
Carlos Andres Escalona Contreras <carlos.escalona@theksquaregroup.com>
2021-05-09 16:23:26 -0500

    Portfolio V1.8.2.2.7.2

--- interactivity.js
+++ interactivity.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  var detailedSections = document.getElementsByClassName('summary-main-section');
+  const detailedSections = document.getElementsByClassName('summary-main-section');
   for(let i = 0; i < detailedSections.length; i++) {
     const hr = `hr${i+1}`;
     const arrow = `arrow${i+1}`;
@@ -10,14 +10,15 @@ window.onload = function() {
       document.getElementById(arrow).classList.toggle("downwards");
     }
   }
-  console.log("Page loaded...");
 }
+
+//DOM unique elements
 const main = document.getElementById("main");
 const logoBtn = document.getElementById("logo");
 const homeBtn = document.getElementById("homeBtn");
 const dropBtn = document.getElementById("dropBtn");
-const sideNavbar = document.getElementById("sideNavbar");
 const topping = document.getElementById("topping");
+const sideNavbar = document.getElementById("sideNavbar");
 const showMenu = document.getElementById("show");
 const hideMenu = document.getElementById("hide");
 const showForm = document.getElementById("showForm");
@@ -25,10 +26,11 @@ const contFormSec = document.getElementById("contactFormSection");
 const contactForm = document.getElementById("contactForm");
 const submitMsg = document.getElementById("submitMsg");
 
-// Get all elements with class="tabcontent"
-description = document.getElementsByClassName("tabcontent");
-for (let i = 0; i < description.length; i++) {
-  description[i].style.zIndex = `${i}`;
+function assignZIndex(name){
+  const description = document.getElementsByName(name);
+  for (let i = 0; i < description.length; i++) {
+    description[i].style.zIndex = `${i}`;
+  }
 }
 function speedUpRotation(){
   const logoSection = document.getElementsByClassName("logo-section");
@@ -77,7 +79,6 @@ function drop() {
   document.getElementById("dropBtn").classList.toggle("active");
   document.getElementById("dropdownContent").classList.toggle("active");
 }
-/*Vertical Tab Gallery (Interests section)*/
 function describe(event, topic){
   if(event.currentTarget.classList.contains("active")){
     // Show the current tab, and add an "active" class to the link that opened the tab
@@ -95,16 +96,20 @@ function describe(event, topic){
     event.currentTarget.classList.add("active");
   }
 }
+function describeMobile(event){
+  event.currentTarget.classList.toggle("active");
+  if(event.currentTarget.classList.contains("active")){
+    event.currentTarget.firstElementChild.classList.replace("fa-chevron-circle-down","fa-chevron-circle-up");
+    return;
+  }
+  event.currentTarget.firstElementChild.classList.replace("fa-chevron-circle-up","fa-chevron-circle-down");
+}
 function showContactForm(){
-  showForm.style.boxShadow = "none";
   showForm.classList.toggle("click");
   setTimeout(() => {
-    showForm.style.boxShadow="8px 8px 5px black";
-  }, 200);
-  setTimeout(() => {
     showForm.classList.toggle("click");
-    contFormSec.style.display = "block";
-    contactForm.style.visibility = "visible";
+    contFormSec.classList.toggle("active");
+    contactForm.classList.toggle("active");
   }, 1100);
 }
 function submitMsgEffect(){
@@ -118,10 +123,10 @@ function submitMsgEffect(){
   }, 1100);
 }
 
-hideMenu.onclick = () => closeNav();
-showMenu.onclick = () => openNav();
-showForm.onclick = () => showContactForm();
-submitMsg.onclick = () => submitMsgEffect();
+// Z-indexes for tabs content:
+assignZIndex("interests");
+assignZIndex("hobbies");
+// Onclick events:
 logoBtn.onclick = () => {
   resetSelection('section-link');
   speedUpRotation();
@@ -131,6 +136,10 @@ homeBtn.onclick = () => {
   clickGlow();
 };
 dropBtn.onclick = () => drop();
+hideMenu.onclick = () => closeNav();
+showMenu.onclick = () => openNav();
+showForm.onclick = () => showContactForm();
+submitMsg.onclick = () => submitMsgEffect();
 
 /*Slideshow (Projects section) */
 var slideIndex = 1;
